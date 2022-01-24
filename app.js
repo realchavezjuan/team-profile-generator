04bed6d47f036e6922fa186b79c4712ad44eddd3
Juan Chavez <juan@Juans-Air.home>
2022-01-23 17:09:17 -0800

    completed app to collect prompt data

--- app.js
+++ app.js
@@ -1,10 +1,122 @@
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
-const promptUser = () => {
+// const promptUser = () => {
+
+//     console.log(`
+//     ======================
+//     Let's Build Your Team!
+//     ======================
+//     `);
+
+//     // prompt for the manager
+//     inquirer.prompt([
+//         {
+//             type: 'text',
+//             name: 'name',
+//             message: 'What is the managers name?'
+//         },
+//         {
+//             type: 'text',
+//             name: 'id',
+//             message: 'What is their id?'
+//         },
+//         {
+//             type: 'text',
+//             name: 'email',
+//             message: 'What is their email?'
+//         },
+//         {
+//             type: 'list',
+//             name: 'office',
+//             message: 'What number is their office room?',
+//             choices: ['Room 101', 'Room 210', 'Room 300']
+//         }
+//     ])
+//     .then(data => {
+//         const manager = new Manager(data.name, data.id, data.email, data.office);
+//         console.log(manager.getRole());
+
+//         // Prompt for Engineers
+//         inquirer.prompt([
+//             {
+//                 type: 'text',
+//                 name: 'name',
+//                 message: 'What is the engineers name?'
+//             },
+//             {
+//                 type: 'text',
+//                 name: 'id',
+//                 message: 'What is their id?'
+//             },
+//             {
+//                 type: 'text',
+//                 name: 'email',
+//                 message: 'What is their email?'
+//             },
+//             {
+//                 type: 'text',
+//                 name: 'office',
+//                 message: 'What number is their GitHub username?'
+//             }
+//         ])
+//         .then(data => {
+//             const engineer = new Engineer(data.name, data.id, data.email, data.github);
+//             inquirer.prompt(
+//                 {
+//                     type: 'confirm',
+//                     name: 'add',
+//                     message: 'Is there another engineer on the team?',
+//                     default: false
+//                 }
+//             )
+//             .then(data){
+//                 if (data){
+
+//                 }
+//             }
+
+
+//             // prompt for the interns
+//             inquirer.prompt([
+//                 {
+//                     type: 'text',
+//                     name: 'name',
+//                     message: 'What is the interns name?'
+//                 },
+//                 {
+//                     type: 'text',
+//                     name: 'id',
+//                     message: 'What is their id?'
+//                 },
+//                 {
+//                     type: 'text',
+//                     name: 'email',
+//                     message: 'What is their email?'
+//                 },
+//                 {
+//                     type: 'list',
+//                     name: 'office',
+//                     message: 'What number is their schools name?',
+//                     choices: ['University of Oregon', 'Oregon State University']
+//                 }
+//             ])
+//             .then(data => {
+//                 const intern = new Intern(data.name, data.id, data.email, data.office);
+//                 console.log(intern.getRole());
+//             });
+//         })
+//     })
+// };
+
+function initialPrompt () {
+
     console.log(`
     ======================
-    Team Profile Generator
+    Let's Build Your Team!
     ======================
     `);
 
@@ -12,7 +124,7 @@ const promptUser = () => {
         {
             type: 'text',
             name: 'name',
-            message: 'What is the name of this employee?'
+            message: 'What is the managers name?'
         },
         {
             type: 'text',
@@ -25,19 +137,107 @@ const promptUser = () => {
             message: 'What is their email?'
         },
         {
-            type: 'checkbox',
-            name: 'role',
-            message: 'What is their role?',
-            choices: ['Manager', 'Engineer', 'Intern'],
+            type: 'list',
+            name: 'office',
+            message: 'What number is their office room?',
+            choices: ['Room 101', 'Room 210', 'Room 300']
         }
     ])
     .then(data => {
-
-        console.log(data);
-
-        const employee = new Employee(data.name, data.id, data.email );
-        console.log(employee);
+        const manager = new Manager(data.name, data.id, data.email, data.office);
+        console.log(manager.getRole());
+        engineersPrompt();
     });
-};
+}
 
-promptUser();
+function engineersPrompt () {
+
+    inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: 'What is the engineers name?'
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: 'What is their id?'
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: 'What is their email?'
+        },
+        {
+            type: 'text',
+            name: 'office',
+            message: 'What number is their GitHub username?'
+        }
+    ])
+    .then(data => {
+        const engineer = new Engineer(data.name, data.id, data.email, data.github);
+        console.log(engineer.getRole());
+        inquirer.prompt(
+            {
+                type: 'confirm',
+                name: 'confirmAdd',
+                message: 'Is there another engineer in your team?'
+            }
+        )
+        .then(data => {
+            // if answer is yes to previous question, repeat the prompt
+            if (data.confirmAdd){
+                engineersPrompt();
+            }
+            else{
+                internPrompt();
+            }
+        })
+    });
+}
+
+function internPrompt () {
+
+    inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: 'What is the interns name?'
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: 'What is their id?'
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: 'What is their email?'
+        },
+        {
+            type: 'list',
+            name: 'school',
+            message: 'What number is their schools name?',
+            choices: ['University of Oregon', 'Oregon State University']
+        }
+    ])
+    .then(data => {
+        const intern = new Intern(data.name, data.id, data.email, data.school);
+        console.log(intern.getRole());
+        inquirer.prompt(
+            {
+                type: 'confirm',
+                name: 'confirmAdd',
+                message: 'Is there another intern in your team?'
+            }
+        )
+        .then(data => {
+            // if answer is yes to previous question, repeat the prompt
+            if (data.confirmAdd){
+                internPrompt();
+            }
+        })
+    });
+}
+
+initialPrompt();
